698f714ea27ab186f2610f37f8fd738d0f3bae80
Jabed <jabedhosenn@gmail.com>
2026-01-24 02:40:03 +0600

    Phase 0

---
+++ database/migrations/2026_01_23_165014_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained('categories')->onDelete('restrict');
+            $table->string('product_name');
+            $table->string('sku')->unique();
+            $table->string('unit'); // pcs, kg, box, etc.
+            $table->decimal('price', 12, 2)->default(0);
+            $table->integer('stock_qty')->default(0);
+            $table->integer('low_stock_threshold')->default(0);
+            $table->string('image_path')->nullable();
+            $table->string('color')->nullable();
+            $table->string('size')->nullable();
+            $table->decimal('weight', 10, 2)->nullable();
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
